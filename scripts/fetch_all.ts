2579dd7854a812e0855b2d1aac6a834b7d10bbde
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-13 13:13:50 +0000

    refactor(scripts): adjust game ID range and order in fetchAll function

--- scripts/fetch_all.ts
+++ scripts/fetch_all.ts
@@ -10,7 +10,7 @@ const fetch_game = async (game_id: number): Promise<void> => {
 }
 
 export async function fetchAll() {
-  const games: number[][] = chunk(Array.from({ length: 3000 }, (_, i) => i + 17000), 25);
+  const games: number[][] = chunk(Array.from({ length: 1000 }, (_, i) => 4000 - i), 25);
 
   for (const chunk_games of games) {
     await Promise.all(chunk_games.map((game) => fetch_game(game)))
