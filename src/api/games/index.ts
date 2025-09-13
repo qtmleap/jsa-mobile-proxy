da2669791c781451505a89d2fe704c06fc715d7e
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-13 19:53:20 +0000

    refactor(api): simplify game search logic and remove unused endpoint

--- src/api/games/index.ts
+++ src/api/games/index.ts
@@ -1,12 +1,9 @@
 import { createRoute, OpenAPIHono } from '@hono/zod-openapi'
-import { decodeGameList, decodeJSA, importJSA } from '@mito-shogi/tsshogi-jsa'
 import { HTTPException } from 'hono/http-exception'
-import { exportJKF, type Record } from 'tsshogi'
 import { z } from 'zod'
 import { GameSchema } from '@/models/game.dto'
-import { SearchListRequestSchema, SearchListResponseSchema } from '@/models/search.dto'
+import { SearchListRequestSchema } from '@/models/search.dto'
 import type { Env } from '@/utils/bindings'
-import { upsertGameInfo } from '@/utils/prisma'
 
 const app = new OpenAPIHono<{ Bindings: Env }>()
 
@@ -33,63 +30,16 @@ app.openapi(
     }
   }),
   async (c) => {
-    const { p1, p2, p3 } = c.req.valid('query')
-    const buffer = await c.env.CLIENT.get('/api/index.php', {
-      queries: {
-        // @ts-ignore
-        action: 'search',
-        p1,
-        p2,
-        p3
-      }
-    })
-    const result = SearchListResponseSchema.safeParse(decodeGameList(Buffer.from(buffer)))
+    const result = z.array(GameSchema).safeParse(
+      await c.env.PRISMA.game.findMany({
+        orderBy: { id: 'desc' },
+        take: 100
+      })
+    )
     if (!result.success) {
-      throw new HTTPException(400, { message: result.error.message })
+      throw new HTTPException(500, { message: result.error.message })
     }
     return c.json(result.data, 200)
-  }
-)
-
-app.openapi(
-  createRoute({
-    method: 'get',
-    path: '/:game_id',
-    tags: ['Search'],
-    summary: 'Search KIF',
-    description: 'Search',
-    request: {
-      params: z.object({
-        game_id: z.coerce.number().int().default(100)
-      })
-    },
-    responses: {
-      200: {
-        content: {
-          'application/json': {
-            // 型の付け方が良くない
-            schema: z.object({})
-          }
-        },
-        description: 'JKF形式の棋譜データ'
-      }
-    }
-  }),
-  async (c) => {
-    const { game_id } = c.req.valid('param')
-    const buffer = await c.env.CLIENT.get('/api/index.php', {
-      queries: {
-        // @ts-ignore
-        action: 'shogi',
-        p1: game_id
-      }
-    })
-    const record: Record | Error = importJSA(buffer)
-    if (record instanceof Error) {
-      throw new HTTPException(400, { message: record.message })
-    }
-    await upsertGameInfo(c.env, decodeJSA(buffer), record)
-    return c.json(exportJKF(record), 200)
   }
 )
 
