b62b078ea12b1b64faf1205fe64972407ff30648
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-13 20:32:10 +0000

    feat(api): include player details in game queries and schema definitions

--- src/models/game.dto.ts
+++ src/models/game.dto.ts
@@ -1,15 +1,17 @@
 import z from 'zod'
 
-export const PlayerSchema = z.object({})
+export const PlayerSchema = z.object({
+  name: z.string().nonempty()
+})
 
 export const GameSchema = z.object({
   id: z.number().int(),
   title: z.string(),
   startTime: z.date(),
   endTime: z.coerce.date().nullable(),
-  moves: z.number().int()
-  // black: PlayerSchema,
-  // white: PlayerSchema
+  moves: z.number().int(),
+  black: PlayerSchema,
+  white: PlayerSchema
 })
 
 export type Player = z.infer<typeof PlayerSchema>
