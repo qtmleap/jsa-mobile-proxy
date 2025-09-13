95f2fc7e660b2f89b38782a968a5a0b456d498a7
tkgstrator <nasawake.am@gmail.com>
2025-09-13 20:43:52 +0000

    feat(players): include game counts and transform schema for aggregation

--- src/models/player.dto.ts
+++ src/models/player.dto.ts
@@ -1,7 +1,18 @@
 import z from 'zod'
 
-export const PlayerSchema = z.object({
-  name: z.string().nonempty()
-})
+export const PlayerSchema = z
+  .object({
+    name: z.string().nonempty(),
+    _count: z
+      .object({
+        blackGames: z.number().int(),
+        whiteGames: z.number().int()
+      })
+      .transform((v) => v.blackGames + v.whiteGames)
+  })
+  .transform((v) => ({
+    name: v.name,
+    count: v._count
+  }))
 
 export type Player = z.infer<typeof PlayerSchema>
