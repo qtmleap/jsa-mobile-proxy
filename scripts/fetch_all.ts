15ceedfe7d6a61a0c317864f3faa505f22564388
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-13 12:46:49 +0000

    feat(api): add upsertGameInfo and update dependencies and schema

---
+++ scripts/fetch_all.ts
@@ -0,0 +1,20 @@
+// 20000件の配列を処理するスクリプト
+
+import { decodeJSA } from "@mito-shogi/tsshogi-jsa"
+import { chunk } from "lodash"
+
+const fetch_game = async (game_id: number): Promise<void> => {
+  const url: URL = new URL(`api/search/${game_id}`,'https://jsa-mobile-proxy.lemonandchan.workers.dev/')
+  await fetch(url.href)
+  console.log(`Fetched game ${game_id}`)
+}
+
+export async function fetchAll() {
+  const games: number[][] = chunk(Array.from({ length: 3000 }, (_, i) => i + 17000), 25);
+
+  for (const chunk_games of games) {
+    await Promise.all(chunk_games.map((game) => fetch_game(game)))
+  }
+}
+
+await fetchAll()
