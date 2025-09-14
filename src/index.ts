6594121faedbd5ac34fd3bb8e2395e635f3149c9
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-14 16:51:46 +0000

    fix(cors): correct origin URLs to include protocol

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ app.use('*', timeout(5000))
 app.use(
   '*',
   cors({
-    origin: ['localhost:5173', 'jsam.qleap.jp'],
+    origin: ['http://localhost:5173', 'https://jsam.qleap.jp'],
     credentials: true,
     maxAge: 86400
   })
