8294bc4a9a041f8b65d94a8d23f4597ac3331e39
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-14 17:19:26 +0000

    feat(models): add OpenAPI metadata to schemas for better API documentation

--- src/models/game.dto.ts
+++ src/models/game.dto.ts
@@ -1,103 +1,109 @@
 import z from 'zod'
 import { Tournament } from '@/enums/tournament'
 
-export const GameSchema = z.object({
-  id: z.number().int().openapi({
-    description: '対局ID',
-    example: 19249
-  }),
-  title: z
-    .string()
-    .openapi({
-      description: 'タイトル',
-      example: '第38期竜王戦6組昇級者決定戦'
-    })
-    .transform((v) => v.replace(/^【[^】]+】\s*/g, '')),
-  startTime: z.date().openapi({
-    description: '開始時刻',
-    example: '2025-09-18T01:00:00.000Z'
-  }),
-  endTime: z.coerce.date().nullable().openapi({
-    description: '終了時刻（進行中の場合は未設定）',
-    example: '2025-09-13T16:30:00Z'
-  }),
-  moves: z.number().int().openapi({
-    description: '総手数',
-    example: 124
-  }),
-  blackId: z.string().nonempty().openapi({
-    description: '先手番の対局者',
-    example: '黒田 尭之'
-  }),
-  whiteId: z.string().nonempty().openapi({
-    description: '後手番の対局者',
-    example: '高橋 佑二郎'
-  }),
-  timeLimit: z.number().int().nullable().openapi({
-    description: '持ち時間（秒）',
-    example: 3600
-  }),
-  kif: z
-    .string()
-    .optional()
-    .openapi({
-      description: 'JKF形式の棋譜データ'
-    })
-    .transform((v) => (v === undefined ? undefined : JSON.parse(v))),
-  tournament: z.string().nonempty().nullable().openapi({
-    description: '棋戦',
-    example: '竜王戦'
-  }),
-  location: z.string().nullable().openapi({
-    description: '対局場所',
-    example: '東京・将棋会館'
-  }),
-  tags: z
-    .array(
-      z
-        .object({
-          name: z.string().nonempty()
-        })
-        .transform((v) => v.name)
-    )
-    .openapi({
-      description: 'タグ',
-      example: ['相掛かり']
-    })
-})
+export const GameSchema = z
+  .object({
+    id: z.number().int().openapi({
+      description: '対局ID',
+      example: 19249
+    }),
+    title: z
+      .string()
+      .openapi({
+        description: 'タイトル',
+        example: '第38期竜王戦6組昇級者決定戦'
+      })
+      .transform((v) => v.replace(/^【[^】]+】\s*/g, '')),
+    startTime: z.date().openapi({
+      description: '開始時刻',
+      example: '2025-09-18T01:00:00.000Z'
+    }),
+    endTime: z.coerce.date().nullable().openapi({
+      description: '終了時刻（進行中の場合は未設定）',
+      example: '2025-09-13T16:30:00Z'
+    }),
+    moves: z.number().int().openapi({
+      description: '総手数',
+      example: 124
+    }),
+    blackId: z.string().nonempty().openapi({
+      description: '先手番の対局者',
+      example: '黒田 尭之'
+    }),
+    whiteId: z.string().nonempty().openapi({
+      description: '後手番の対局者',
+      example: '高橋 佑二郎'
+    }),
+    timeLimit: z.number().int().nullable().openapi({
+      description: '持ち時間（秒）',
+      example: 3600
+    }),
+    kif: z
+      .string()
+      .optional()
+      .openapi({
+        description: 'JKF形式の棋譜データ'
+      })
+      .transform((v) => (v === undefined ? undefined : JSON.parse(v))),
+    tournament: z.string().nonempty().nullable().openapi({
+      description: '棋戦',
+      example: '竜王戦'
+    }),
+    location: z.string().nullable().openapi({
+      description: '対局場所',
+      example: '東京・将棋会館'
+    }),
+    tags: z
+      .array(
+        z
+          .object({
+            name: z.string().nonempty()
+          })
+          .transform((v) => v.name)
+      )
+      .openapi({
+        description: 'タグ',
+        example: ['相掛かり']
+      })
+  })
+  .openapi('GameSchema')
 
 export type Game = z.infer<typeof GameSchema>
 
-export const GameRequestParamsSchema = z.object({
-  game_id: z.coerce.number().int().openapi({
-    description: '対局ID',
-    example: 100
+export const GameRequestParamsSchema = z
+  .object({
+    game_id: z.coerce.number().int().openapi({
+      description: '対局ID',
+      example: 100
+    })
   })
-})
+  .openapi('GameRequestParamsSchema')
 
-export const GameRequestQuerySchema = z.object({
-  page: z.coerce.number().int().min(1).default(1).openapi({
-    description: 'ページ番号',
-    example: 1
-  }),
-  limit: z.coerce.number().int().min(1).max(100).default(20).openapi({
-    description: '1ページあたりの取得件数',
-    example: 20
-  }),
-  tournament: z.enum(Tournament).optional().openapi({
-    description: '棋戦名でフィルタリング',
-    example: '竜王戦'
-  }),
-  player: z.string().nonempty().optional().openapi({
-    description: '対局者名でフィルタリング',
-    example: '黒田 尭之'
-  }),
-  startTime: z.coerce.date().optional().openapi({
-    description: '開始時刻以降の対局でフィルタリング',
-    example: '2025-09-01'
-  }),
-  endTime: z.coerce.date().optional().openapi({
-    description: '終了時刻以前の対局でフィルタリング',
-    example: '2025-09-30'
+export const GameRequestQuerySchema = z
+  .object({
+    page: z.coerce.number().int().min(1).default(1).openapi({
+      description: 'ページ番号',
+      example: 1
+    }),
+    limit: z.coerce.number().int().min(1).max(100).default(20).openapi({
+      description: '1ページあたりの取得件数',
+      example: 20
+    }),
+    tournament: z.enum(Tournament).optional().openapi({
+      description: '棋戦名でフィルタリング',
+      example: '竜王戦'
+    }),
+    player: z.string().nonempty().optional().openapi({
+      description: '対局者名でフィルタリング',
+      example: '黒田 尭之'
+    }),
+    startTime: z.coerce.date().optional().openapi({
+      description: '開始時刻以降の対局でフィルタリング',
+      example: '2025-09-01'
+    }),
+    endTime: z.coerce.date().optional().openapi({
+      description: '終了時刻以前の対局でフィルタリング',
+      example: '2025-09-30'
+    })
   })
-})
+  .openapi('GameRequestQuerySchema')
--- src/models/search.dto.ts
+++ src/models/search.dto.ts
@@ -1,44 +1,58 @@
 import { z } from '@hono/zod-openapi'
 
-export const SearchListRequestSchema = z.object({
-  action: z.literal('search').default('search'),
-  p1: z.coerce.number().int().default(0),
-  p2: z.coerce.number().int().default(14000),
-  p3: z.coerce.number().int().default(3)
-})
-
-export const SearchGameRequestSchema = z.object({
-  action: z.literal('shogi').default('shogi'),
-  p1: z.number().int().default(100)
-})
-
-export const SearchRequestSchema = z.discriminatedUnion('action', [SearchListRequestSchema, SearchGameRequestSchema])
-
-export const SearchItemPlayerSchema = z.object({
-  last_name: z.string(),
-  first_name: z.string(),
-  rank: z.string()
-})
-
-export const SearchItemResponseSchema = z.object({
-  game_id: z.number().int(),
-  start_time: z.coerce.date(),
-  end_time: z.coerce.date().nullable(),
-  title: z.string(),
-  tournament: z.string().optional(),
-  moves: z.number().int(),
-  black: SearchItemPlayerSchema,
-  white: SearchItemPlayerSchema
-})
-
-export const SearchListResponseSchema = z.object({
-  games: z.array(SearchItemResponseSchema),
-  count: z.number().int().positive()
-})
-
-export const SearchRequestParamsSchema = z.object({
-  game_id: z.coerce.number().int().openapi({
-    description: '対局ID',
-    example: 100
+export const SearchListRequestSchema = z
+  .object({
+    action: z.literal('search').default('search'),
+    p1: z.coerce.number().int().default(0),
+    p2: z.coerce.number().int().default(14000),
+    p3: z.coerce.number().int().default(3)
   })
-})
+  .openapi('SearchListRequestSchema')
+
+export const SearchGameRequestSchema = z
+  .object({
+    action: z.literal('shogi').default('shogi'),
+    p1: z.number().int().default(100)
+  })
+  .openapi('SearchGameRequestSchema')
+
+export const SearchRequestSchema = z
+  .discriminatedUnion('action', [SearchListRequestSchema, SearchGameRequestSchema])
+  .openapi('SearchRequestSchema')
+
+export const SearchItemPlayerSchema = z
+  .object({
+    last_name: z.string(),
+    first_name: z.string(),
+    rank: z.string()
+  })
+  .openapi('SearchItemPlayerSchema')
+
+export const SearchItemResponseSchema = z
+  .object({
+    game_id: z.number().int(),
+    start_time: z.coerce.date(),
+    end_time: z.coerce.date().nullable(),
+    title: z.string(),
+    tournament: z.string().optional(),
+    moves: z.number().int(),
+    black: SearchItemPlayerSchema,
+    white: SearchItemPlayerSchema
+  })
+  .openapi('SearchItemResponseSchema')
+
+export const SearchListResponseSchema = z
+  .object({
+    games: z.array(SearchItemResponseSchema),
+    count: z.number().int().positive()
+  })
+  .openapi('SearchListResponseSchema')
+
+export const SearchRequestParamsSchema = z
+  .object({
+    game_id: z.coerce.number().int().openapi({
+      description: '対局ID',
+      example: 100
+    })
+  })
+  .openapi('SearchRequestParamsSchema')
