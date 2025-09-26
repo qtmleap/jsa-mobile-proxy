4d7ed0f610cfb4cecd5a9f66b6cf5134446c6c83
tkgstrator <nasawake.am@gmail.com>
2025-09-26 06:17:23 +0000

    fix(api): ensure upsertGameInfo is awaited with execution context

--- scripts/fetch_all.ts
+++ scripts/fetch_all.ts
@@ -4,13 +4,13 @@ import { decodeJSA } from "@mito-shogi/tsshogi-jsa"
 import { chunk } from "lodash"
 
 const fetch_game = async (game_id: number): Promise<void> => {
-  const url: URL = new URL(`api/search/${game_id}`,'https://jsam.qleap.jp/')
-  const response = await fetch(url.href)
-  console.log(`Fetched game ${game_id} status: ${response.status}`)
+  const url: URL = new URL(`api/search/${game_id}`,'https://jsa-mobile-proxy-prod.lemonandchan.workers.dev/')
+  await fetch(url.href)
+  console.log(`Fetched game ${game_id}`)
 }
 
 export async function fetchAll() {
-  const games: number[][] = chunk(Array.from({ length: 200 }, (_, i) => i), 25);
+  const games: number[][] = chunk(Array.from({ length: 20000 }, (_, i) => i), 25);
 
   for (const chunk_games of games) {
     await Promise.all(chunk_games.map((game) => fetch_game(game)))
