956cf32dd4bf65f25ccacac11f9b2dd79a682d8d
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-27 03:33:23 +0000

    feat(scripts): add authorization header to fetch_game requests

--- scripts/fetch_all_ai.ts
+++ scripts/fetch_all_ai.ts
@@ -8,8 +8,15 @@ import { fileURLToPath } from "node:url"
 
 const fetch_game = async (game_id: number): Promise<void> => {
   const url: URL = new URL(`ai/${game_id}.json`,'https://d2pngvm764jm.cloudfront.net')
-  const { status } = await fetch(url.href)
-  console.log(`Fetched game ${game_id} with status ${status}`)
+  const response = await fetch(url.href, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Basic YXdzX3Nob2dpOlNob2dpTGl2ZQ=='
+    }
+  })
+  const data = await response.json()
+  console.log(data)
+  // console.log(`Fetched game ${game_id} with status ${status}`)
 }
 
 export async function fetchAll() {
@@ -23,6 +30,7 @@ export async function fetchAll() {
 
   for (const chunk_games of games) {
     await Promise.all(chunk_games.map((game) => fetch_game(game)))
+    break
   }
 }
 
