6cece6ec93d5074d1f8430e673096b456a9a23cc
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-27 19:06:46 +0000

    refactor(tests): simplify imports and update file paths in test cases

--- __tests__/ai/index.test.ts
+++ __tests__/ai/index.test.ts
@@ -1,21 +1,12 @@
 import { describe } from 'bun:test'
 import { doesNotThrow } from 'node:assert'
-import { readFileSync } from 'node:fs'
-import { dirname } from 'node:path'
-import { join } from 'node:path/posix'
 import { it } from 'node:test'
-import { fileURLToPath } from 'bun'
 import { GameJSONSchema } from '@/models/ai.dto'
-
-const readJSONSync = (filePath: string): any => {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-  return JSON.parse(readFileSync(join(__dirname, filePath), 'utf8'))
-}
+import { readJSONSync } from '../client'
 
 describe('Equality', () => {
-  const _expected = readJSONSync('ip.jsamobile.jp/18440.json')
-  const received = readJSONSync('d2pngvm764jm.cloudfront.net/18440.json')
+  const _expected = readJSONSync('ai/ip.jsamobile.jp/18440.json')
+  const received = readJSONSync('ai/d2pngvm764jm.cloudfront.net/18440.json')
 
   it('Decode To Record', () => {
     doesNotThrow(() => GameJSONSchema.parse(received))
