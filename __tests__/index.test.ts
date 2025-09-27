6e7fe609af968ab4de63016602fa1516d80b876c
tkgstrator <nasawake.am@gmail.com>
2025-09-27 05:05:30 +0000

    chore(tests): update test files and adjust AI DTO model

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,8 +1,10 @@
+import { GameJSONSchema } from "@/models/ai.dto";
 import { fileURLToPath } from "bun";
 import { describe } from "bun:test";
-import { readFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 import { dirname } from "node:path";
 import { join } from "node:path/posix";
+import { it } from "node:test";
 
 const readJSONSync = (filePath: string): any => {
   const __filename = fileURLToPath(import.meta.url)
@@ -14,5 +16,8 @@ describe("Equality", () => {
   const expected = readJSONSync("ip.jsamobile.jp/18440.json")
   const received = readJSONSync("d2pngvm764jm.cloudfront.net/18440.json")
 
-
+  it('Decode To Record', () => {
+    const result = GameJSONSchema.parse(received)
+    writeFileSync("output.json", JSON.stringify(result, null, 2))
+  })
 })
